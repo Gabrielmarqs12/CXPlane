1cef19dfbebd3c87846e74cc036e556fa1d6e3ae
Gabriel Marques Cardoso <98746946+Gabrielmarqs12@users.noreply.github.com>
2023-07-27 20:29:07 -0300

    Update of procedure.py
    Added the function frequency_selector()

--- lib/procedure.py
+++ lib/procedure.py
@@ -18,6 +18,23 @@ def start(info):
 		print("[V] Information logged")
 	pass
 
+def frequency_select(frequency):
+    with open("FreqList", "r") as freq
+    nbr = freq.readlines()
+        for ln in nbr:
+            data.split(":")
+            data[1] = data[1].replace("\n", "")
+            if data[0] == frequency:
+                if data[1] = dep:
+                return("[V]frequency selected successfully ")
+                elif "Approach" in data[3]:
+                return("[V]frequency selected successfully ")
+                else
+                return("[!]Frequency out of range")
+            else
+            return ("[!]frequency not found")
+pass
+
 def recognize(message):
 	if gnd in message:
 		if req in message:
